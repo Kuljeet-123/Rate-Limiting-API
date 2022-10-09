469f66ee4014c3e6d7e31d65fb8010ef51b01ce3
Kuljeet Singh <kuljeet@32nd.com>
2022-10-09 14:22:15 +0530

    rate limiting api using express-rate-limi in nodejs

---
+++ README.md
@@ -0,0 +1,7 @@
+## Rate Limiting API
+
+**Testing API with Universities big data**
+
+* here I have used only max and windowMs with rate-limiting.
+* max - means maximum no. of requests within ***windowMs*** of time.
+* windowMs - time in ms
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const universities = require('./universities');
+const rateLimt = require('express-rate-limit');
+
+const limiter = rateLimt({
+    max: 5,
+    windowMs: 10000
+})
+
+app.get('/universities', limiter, (req, res)=>{
+    res.send({
+        status:'Success',
+        universities: universities
+    })
+})
+
+app.listen(9000, () => console.log('Listen on port 9000'));
